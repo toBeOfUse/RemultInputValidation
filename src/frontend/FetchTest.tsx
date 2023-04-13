c61bbf70cc1497cb16b6bd74ff7748cfe3ac8d36
Mitch J <mjacovet@kent.edu>
2023-04-13 17:08:09 -0400

    add database post test page

---
+++ src/frontend/FetchTest.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+
+export default function App() {
+  const [toPost, setToPost] = useState(
+    JSON.stringify(
+      { title: 'test insert', completed: false, shouldBeString: 89 },
+      null,
+      4
+    )
+  )
+
+  const [currentDB, setCurrentDB] = useState('')
+  const getCurrentDB = async () => {
+    setCurrentDB(
+      JSON.stringify(await (await fetch('/api/tasks')).json(), null, 4)
+    )
+  }
+  useEffect(() => {
+    getCurrentDB()
+  }, [])
+
+  const [currentError, setCurrentError] = useState('')
+  const postToDB = async () => {
+    try {
+      const res = await fetch('/api/tasks', {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: toPost
+      })
+      if (!res.ok) {
+        throw JSON.stringify(await res.json())
+      }
+      setCurrentError('')
+      await getCurrentDB()
+    } catch (e: any) {
+      setCurrentError(e.toString())
+    }
+  }
+
+  return (
+    <>
+      <h3>Enter JSON to test adding to the database:</h3>
+      <textarea
+        style={{ display: 'block', width: 500, minHeight: 200 }}
+        value={toPost}
+        onChange={(e) => setToPost(e.target.value)}
+      />
+      <button onClick={postToDB}>
+        Post to <pre style={{ display: 'inline' }}>/api/tasks</pre>
+      </button>
+      {currentError && (
+        <>
+          <h4>Error:</h4>
+          <pre>{currentError}</pre>
+        </>
+      )}
+      <h2>Current DB entries:</h2>
+      <pre>{currentDB}</pre>
+    </>
+  )
+}
